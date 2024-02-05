e260ad0b97926559cbd7e190b3978bb4f657d46c
Elohyrr <118767910+Elohyrr@users.noreply.github.com>
2024-02-05 15:27:26 +0100

    changement du script powershell en batch

---
+++ script2.bat
@@ -0,0 +1,32 @@
+@echo off
+
+:: Récupérer le nom de l'hôte
+set hostName=%COMPUTERNAME%
+
+:: Récupérer le système d'exploitation
+for /F "delims=" %%i in ('wmic os get caption ^| findstr /r "."') do set osInfo=%%i
+
+:: Récupérer la version du noyau
+for /F "delims=" %%i in ('wmic os get version ^| findstr /r "."') do set kernelVersion=%%i
+
+:: Récupérer les informations sur le processeur
+for /F "delims=" %%i in ('wmic cpu get name ^| findstr /r "."') do set cpuInfo=%%i
+
+:: Récupérer la quantité de mémoire RAM disponible
+for /F "delims=" %%i in ('wmic computersystem get totalphysicalmemory ^| findstr /r "."') do set ramInfo=%%i
+
+:: Afficher les informations récupérées
+echo Informations système pour %hostName%
+echo Système d'exploitation  %osInfo%
+echo Version du noyau  %kernelVersion%
+echo Processeur  %cpuInfo%
+echo Mémoire RAM (en Go)  %ramInfo%
+
+:: Enregistrer les informations dans un fichier texte
+echo Informations système pour %hostName% > "%temp%\system_info.txt"
+echo Système d'exploitation  %osInfo% >> "%temp%\system_info.txt"
+echo Version du noyau  %kernelVersion% >> "%temp%\system_info.txt"
+echo Processeur  %cpuInfo% >> "%temp%\system_info.txt"
+echo Mémoire RAM (en Go)  %ramInfo% >> "%temp%\system_info.txt"
+
+pause
