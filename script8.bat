fcda0b006fedde92196ec8f9dd6ed1c7b23c9c3e
TcLVG <151175285+TcLVG@users.noreply.github.com>
2024-02-06 17:30:39 +0100

    Update script8.bat

--- script8.bat
+++ script8.bat
@@ -3,17 +3,11 @@
 rem URL de la vidéo YouTube
 set "video_url=https://www.youtube.com/watch?v=gagePw3QA3U"
 
-rem Augmente le volume à fond
-nircmd.exe setsysvolume 65535
-
 rem Créer un fichier PowerShell temporaire pour ouvrir l'URL dans le navigateur
 echo Start-Process '%video_url%' -Wait > open_video.ps1
 
 rem Exécuter le script PowerShell
 powershell -ExecutionPolicy Bypass -File open_video.ps1
-
-timeout /t 3 /nobreak >nul
-nircmd sendkeypress f
 
 rem Supprimer le script PowerShell temporaire
 del open_video.ps1
