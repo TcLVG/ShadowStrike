66ff440f9078f81a83bb16eda5342b53e337e8f3
TcLVG <151175285+TcLVG@users.noreply.github.com>
2024-02-06 16:17:46 +0100

    Update script8.bat

--- script8.bat
+++ script8.bat
@@ -3,6 +3,9 @@
 rem URL de la vidéo YouTube
 set "video_url=https://www.youtube.com/watch?v=gagePw3QA3U"
 
+rem Augmente le volume à fond
+nircmd.exe setsysvolume 65535
+
 rem Créer un fichier PowerShell temporaire pour ouvrir l'URL dans le navigateur
 echo Start-Process '%video_url%' -Wait > open_video.ps1
 
