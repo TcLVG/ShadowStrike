a3ca38371ba94752582f86c3eba48b8a61830082
Elohyrr <118767910+Elohyrr@users.noreply.github.com>
2024-02-06 10:45:53 +0100

    clin d'oeil

---
+++ script7.bat
@@ -0,0 +1,125 @@
+@echo off
+mode con:cols=83 lines=41
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	       .,,.                                                                   
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	       ,KMKkkkkkkkklll. ..                             ..       .,,,.         
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	    .lkKKl,,,,,,,lkkKMKkKkll. ..                 ..,l,lKKkkkkkkkKMMKl.        
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   .lKKl.           .,,lkkKMKkKk,. ,l.  ,l. ,l. .kKKKkkl,,,,,,,,,lkKMKkl.     
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .,kMk.   .,,,,,.         .,,lkKMKkKMKkkKMklKMKkKKl,.       .,,,.  .,kMKl.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .kMk.    .,lkKMKkkl.        ...,lKMMk,llkMMklKKl.  .... .lkKKl,.    .kMl     
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .kMl         .,,lKMKkl.  .. lKl. .ll.   .ll. ..  ,lkKKklKMKl.        lmk.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	.kMk.             .lKMMKllKKkKKKl                .kMKKMMMMMl..        lmk.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 ,KK,              .KMKKKKklkKl..                 ,l..,lKKKKKx,l,    ,KKl.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	.lKK,          ..,,lMK,....  ..                         ..lKkKMMl .. lMKl.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 lMl         ..lKKKKMK,                                   .. lKKKlkl .kMk.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	.kMl         lKKKKMk,.                                       ...lKMK, lMk.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 lMk.      ,lkMk.,l.                                             ,lkl,KK,     
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 ,KMl     .kkll,                     .,.                ..         ..,Kk,.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .kMk.    lMl      .lkkkkkkkl,,.    ,KMl                lk,     .lkkkkKMMK,   
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	  ,KMl    ,l.     .lklkMk,,,lKMKkl,lKMMl                lMKl,,lkkkl,,kMMMMk.  
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   lMK.               lMl    .,lkkKMKl,.                .lKMMKkl.   .kMKKMK,  
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   ,KMKl.             lMk.       .kMl                     ,KMk.  .,lKKl.,KMk. 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .,lKKkl.             ,KMKl,,,,,lKKl.      .lkkkkkkkkkl.   ,KMKkkKKkl.  lMK,  
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 ,KMKk,.               .lkkkkkkkkl.        lMMMMMMMMMMMl    .,,,,,.     ,KMk. 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .lKMMK,                                   .kMMMMMMMMMMl                 lMMl 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	  .lkKMk..l,                .ll,.           .lKMMMMMMkl.       .,ll.     lMK. 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .,,lKKl.lMKkl.            ,KK,               .,lKMKl.           ,KK,    lMK, 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .lKMMk,..,,,,.            lMl                  .kMl              lMl   ,KMMK,
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   ,KMMK.   ,Kl            lMl          .,,,,,lkKMMKkl,,.         lMl   lMKkl.
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	  .kMkl.   ,KK,            lMl          .kMMMMKKMMMMKKMMKl.      ,KMl   lMKl. 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	  .lkKKkkl.lMKkkl.         ,KKl.         .lkKMkkMKKMkkMk,.     .lKMk.   .kMMl 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	     .lKMMl.,,,,. ..        ,KMKkl.         .lKKl..lKKl.      .kKkl.   ,kKMK, 
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	      .kMMKk,    .kl.,,.     .,,lkl.   ..     ..    ..  ..   .ll.   .,,kMMk.  
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	     .lKMMMKl,,,.lMKKKl.               lKl.           .lKl          lMKKMK,   
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	    .kMMKkkkkkKMl.lkk,                 .kMKl,.     .,lKKl.       .lkKMl.,.    
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   .lKMMk.    lMKkKMMKk.,,              .lkKMKkkkkkKMKl.      ,k,lKKMK,       
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	     ,KKl. .. .,,,,kMKl.lK, .,,.           .,,,,,,,,,.   ,;,lkKMKk,.,.        
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   .lKMk. .kl.l,   .,.  lMKkKKKklkk,.,,,,.             .,kMMKKMk,.            
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   .lKMK,.kMKkk,        ,kl,,.lMKlkKKKkKMK,.,,lllllkkkkkKKll,lMl              
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	   .lKMk..,,,.                .,. lKl. .ll.lKKMKkl,lKKl...  .kMKl.            
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	  .lKMk.                   ,l. .. ..       ...l,    ..     ,KMKl,.            
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	  ,KMKl.  ..               lMklKl                          lMMk.              
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .lKMkl.  lk.,l.  .lx,     ,kMKl.                                             
+pathping -h 1 -p 10 -q 1 -w 1 127.0.0.1>nul
+echo	 .lKk.    lMKKMl lKKl                                                         
+TIMEOUT 2 /NOBREAK>NUL
+cls
+echo	       .,,.                                                                   
+echo	       ,KMKkkkkkkkklll. ..                             ..       .,,,.         
+echo	    .lkKKl,,,,,,,lkkKMKkKkll. ..                 ..,l,lKKkkkkkkkKMMKl.        
+echo	   .lKKl.           .,,lkkKMKkKk,. ,l.  ,l. ,l. .kKKKkkl,,,,,,,,,lkKMKkl.     
+echo	 .,kMk.   .,,,,,.         .,,lkKMKkKMKkkKMklKMKkKKl,.       .,,,.  .,kMKl.    
+echo	 .kMk.    .,lkKMKkkl.        ...,lKMMk,llkMMklKKl.  .... .lkKKl,.    .kMl     
+echo	 .kMl         .,,lKMKkl.  .. lKl. .ll.   .ll. ..  ,lkKKklKMKl.        lmk.    
+echo	.kMk.             .lKMMKllKKkKKKl                .kMKKMMMMMl..        lmk.    
+echo	 ,KK,              .KMKKKKklkKl..                 ,l..,lKKKKKx,l,    ,KKl.    
+echo	.lKK,          ..,,lMK,....  ..                         ..lKkKMMl .. lMKl.    
+echo	 lMl         ..lKKKKMK,                                   .. lKKKlkl .kMk.    
+echo	.kMl         lKKKKMk,.                                       ...lKMK, lMk.    
+echo	 lMk.      ,lkMk.,l.                                             ,lkl,KK,     
+echo	 ,KMl     .kkll,                                        ..         ..,Kk,.    
+echo	 .kMk.    lMl                                           lk,     .lkkkkKMMK,   
+echo	  ,KMl    ,l.                        .,,l,              lMKl,,lkkkl,,kMMMMk.  
+echo	   lMK.                .,,,,,,,,,lkkkKMMK,              .lKMMKkl.   .kMKKMK, 
+echo	   ,KMKl.            ,kKMMKkkkkkkkkkkkkk,                 ,KMk.  .,lKKl.,KMk. 
+echo	 .,lKKkl.            .,,,,.                .lkkkkkkkkkl.   ,KMKkkKKkl.  lMK,  
+echo	 ,KMKk,.                                   lMMMMMMMMMMMl    .,,,,,.     ,KMk. 
+echo	 .lKMMK,                                   .kMMMMMMMMMMl                 lMMl 
+echo	  .lkKMk..l,                .ll,.           .lKMMMMMMkl.       .,ll.     lMK. 
+echo	 .,,lKKl.lMKkl.            ,KK,               .,lKMKl.           ,KK,    lMK, 
+echo	 .lKMMk,..,,,,.            lMl                  .kMl              lMl   ,KMMK,
+echo	   ,KMMK.   ,Kl            lMl          .,,,,,lkKMMKkl,,.         lMl   lMKkl.
+echo	  .kMkl.   ,KK,            lMl          .kMMMMKKMMMMKKMMKl.      ,KMl   lMKl. 
+echo	  .lkKKkkl.lMKkkl.         ,KKl.         .lkKMkkMKKMkkMk,.     .lKMk.   .kMMl 
+echo	     .lKMMl.,,,,. ..        ,KMKkl.         .lKKl..lKKl.      .kKkl.   ,kKMK, 
+echo	      .kMMKk,    .kl.,,.     .,,lkl.   ..     ..    ..  ..   .ll.   .,,kMMk.  
+echo	     .lKMMMKl,,,.lMKKKl.               lKl.           .lKl          lMKKMK,   
+echo	    .kMMKkkkkkKMl.lkk,                 .kMKl,.     .,lKKl.       .lkKMl.,.    
+echo	   .lKMMk.    lMKkKMMKk.,,              .lkKMKkkkkkKMKl.      ,k,lKKMK,       
+echo	     ,KKl. .. .,,,,kMKl.lK, .,,.           .,,,,,,,,,.   ,;,lkKMKk,.,.        
+echo	   .lKMk. .kl.l,   .,.  lMKkKKKklkk,.,,,,.             .,kMMKKMk,.            
+echo	   .lKMK,.kMKkk,        ,kl,,.lMKlkKKKkKMK,.,,lllllkkkkkKKll,lMl              
+echo	   .lKMk..,,,.                .,. lKl. .ll.lKKMKkl,lKKl...  .kMKl.            
+echo	  .lKMk.                   ,l. .. ..       ...l,    ..     ,KMKl,.            
+echo	  ,KMKl.  ..               lMklKl                          lMMk.              
+echo	 .lKMkl.  lk.,l.  .lx,     ,kMKl.                                             
+echo	 .lKk.    lMKKMl lKKl                                                         
+pathping -h 1 -p 500 -q 1 -w 1 127.0.0.1>nul
