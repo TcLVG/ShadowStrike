89431a9e1619789761c838921f1ee8c1bc0e0d7c
TcLVG <151175285+TcLVG@users.noreply.github.com>
2024-02-06 16:07:49 +0100

    test

--- script8.bat
+++ script8.bat
@@ -1,21 +1,14 @@
 @echo off
+setlocal
 
-rem URL de l'image sur GitHub
-set "image_url=https://github.com/TcLVG/ShadowStrike/blob/main/shrek.jpg"
+rem Spécifiez le chemin du navigateur web (Chrome) sur votre système
+set "navigateur=C:\Program Files\Google\Chrome\Application\chrome.exe"
 
-rem Chemin où sauvegarder temporairement l'image
-set "temp_image=C:\temp\image.jpg"
+rem Lien vers la vidéo YouTube
+set "lien_video=https://www.youtube.com/watch?v=gagePw3QA3U"
 
-rem Téléchargement de l'image depuis GitHub
-curl -o "%temp_image%" "%image_url%"
+rem Lance le navigateur avec le lien vidéo
+"%navigateur%" "%lien_video%"
 
-rem Commande pour changer le fond d'écran
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "%temp_image%" /f
+endlocal
 
-rem Actualisation du fond d'écran
-rundll32.exe user32.dll, UpdatePerUserSystemParameters
-
-echo Fond d'écran changé avec succès.
-
-rem Suppression de l'image temporaire
-del "%temp_image%"
