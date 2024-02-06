f6b908c075ee581986a53af77f5e9b79943c2f8f
Elohyrr <118767910+Elohyrr@users.noreply.github.com>
2024-02-06 15:46:17 +0100

    rick

--- script8.bat
+++ script8.bat
@@ -3,11 +3,5 @@
 rem URL de la vidéo YouTube
 set "video_url=https://youtu.be/dQw4w9WgXcQ"
 
-rem Téléchargement de la vidéo avec youtube-dl
-youtube-dl -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" -o video.mp4 %video_url%
-
-rem Lecture de la vidéo avec mpv
-mpv video.mp4
-
-rem Suppression de la vidéo après la lecture
-del video.mp4
+rem Ouvrir la vidéo dans le navigateur par défaut
+start "" %video_url%
