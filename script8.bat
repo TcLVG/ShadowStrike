292cff490b2a1b0028e2f4cd85480325a891524b
TcLVG <151175285+TcLVG@users.noreply.github.com>
2024-02-06 16:12:14 +0100

    Update script8.bat

--- script8.bat
+++ script8.bat
@@ -1,7 +1,7 @@
 @echo off
 
 rem URL de la vidéo YouTube
-set "video_url=https://youtu.be/dQw4w9WgXcQ"
+set "video_url=https://www.youtube.com/watch?v=gagePw3QA3U"
 
 rem Créer un fichier PowerShell temporaire pour ouvrir l'URL dans le navigateur
 echo Start-Process '%video_url%' -Wait > open_video.ps1
